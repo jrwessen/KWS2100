cf48faa19f9eeb431b49f8842562558346228654
Jaruwan Essén <jaruwan.essen@gmail.com>
2024-01-26 11:17:35 +0100

    Display kommuneLayerCheckbox

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -1,4 +1,4 @@
-import React, {MutableRefObject, useEffect, useRef} from "react";
+import React, {MutableRefObject, useEffect, useRef, useState} from "react";
 import {Map, View} from "ol";
 import TileLayer from "ol/layer/Tile";
 import {OSM} from "ol/source";
@@ -12,7 +12,7 @@ useGeographic();
 
 //TO show map
 const map = new Map({
-    layers: [new TileLayer({ source: new OSM() })],
+
     view: new View({center: [10,59], zoom: 8}),
 });
 
@@ -26,9 +26,9 @@ export function Application() {
                 center: [longitude, latitude], zoom:10,
             });
         });
-
     }
 
+    const[layers, setLayers] = useState(new TileLayer({ source: new OSM}))
     const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
     useEffect(() => {
         map.setTarget(mapRef.current);
